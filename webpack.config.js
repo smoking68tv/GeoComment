84ee431865d1743e68d2e77b90b7a61919b1830e
Alexey <100.1000@list.ru>
2019-06-22 17:29:08 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js'
+  },
+  devtool: 'sourse-map',
+  module:{
+    rules:[
+        {
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'js/js'),
+            use:{
+                loader: 'babel-loader',
+                options:{
+                    presets: 'env'
+                }
+            }
+            
+        }
+    ]
+},
+};
